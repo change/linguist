df743647ad8d9f9a6fde913c126b54a2c8977d34
Chris McCord <chris@chrismccord.com>
2014-06-27 20:58:24 -0400

    Add Hex config

--- mix.exs
+++ mix.exs
@@ -1,11 +1,23 @@
+Code.ensure_loaded?(Hex) and Hex.start
+
 defmodule Linguist.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :linguist,
-     version: "0.0.1",
-     elixir: "~> 0.14.1",
-     deps: deps]
+    [
+      app: :linguist,
+      version: "0.0.1",
+      elixir: "~> 0.14.1",
+      deps: deps,
+      package: [
+        contributors: ["Chris McCord"],
+        licenses: ["MIT"],
+        links: [github: "https://github.com/chrismccord/linguist"]
+      ],
+      description: """
+      Elixir Internationalization library
+      """
+    ]
   end
 
   # Configuration for the OTP application
