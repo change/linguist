6f3d8937906ab2a8f65382ca8f638145ad118419
Chris McCord <chris@chrismccord.com>
2015-03-02 21:26:52 -0500

    Bump version

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Linguist.Mixfile do
   def project do
     [
       app: :linguist,
-      version: "0.1.4",
+      version: "0.1.5",
       elixir: "~> 1.0",
       deps: deps,
       package: [
