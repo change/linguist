1d416d129de6f43c63b687fe2efc8d6bd0220d9c
Alexander Rozumii <alexander.rozumii@toptal.com>
2016-06-28 14:50:33 +0300

    Fix Elixir 1.3 warnings

--- lib/linguist/vocabulary.ex
+++ lib/linguist/vocabulary.ex
@@ -66,9 +66,11 @@ defmodule Linguist.Vocabulary do
   """
   defmacro locale(name, source) do
     quote bind_quoted: [name: name, source: source] do
-      if is_binary(source) do
+      source = if is_binary(source) do
         @external_resource source
-        source = Code.eval_file(source) |> elem(0)
+        Code.eval_file(source) |> elem(0)
+      else
+        source
       end
       @locales {name, source}
     end
