44521551377427421acefb639f4a6ea3efea60d4
Will Barrett <wbarrett@change.org>
2018-04-19 14:56:06 -0700

    Lookin' like it's working!

--- test/memorized_vocabulary_test.exs
+++ test/memorized_vocabulary_test.exs
@@ -6,19 +6,29 @@ defmodule MemorizedVocabularyTest do
     {:ok, foo: foo}
   end
 
-  test "it returns locales" do
+  test "locales() returns locales" do
     assert ["es"] == Linguist.MemorizedVocabulary.locales()
   end
 
-  test "it returns a translation" do
+  test "t returns a translation" do
     assert {:ok, "bar"} == Linguist.MemorizedVocabulary.t("es", "foo")
   end
 
-  test "it interpolates values" do
+  test "t interpolates values" do
     assert {:ok, "hola Michael Westin"} == Linguist.MemorizedVocabulary.t("es", "flash.notice.hello", first: "Michael", last: "Westin")
   end
 
-  test "it pluralizes" do
+  test "t returns {:error, :no_translation} when translation is missing" do
+    assert Linguist.MemorizedVocabulary.t("es", "flash.not_exists") == {:error, :no_translation}
+  end
+
+  test "t! raises NoTranslationError when translation is missing" do
+    assert_raise Linguist.NoTranslationError, fn ->
+      Linguist.MemorizedVocabulary.t!("es", "flash.not_exists")
+    end
+  end
+
+  test "t pluralizes" do
     assert {:ok, "2 manzanas"} == Linguist.MemorizedVocabulary.t("es", "apple", count: 2)
   end
 end
