1d16809ee2f51d2836ddd364125d9b729dedffb9
John Mertens <john.mertens@gmail.com>
2020-04-27 22:02:37 -0700

    docs: Add ex_doc, clean up the generated documentation

--- mix.exs
+++ mix.exs
@@ -3,21 +3,34 @@ Code.ensure_loaded?(Hex) and Hex.start()
 defmodule Linguist.Mixfile do
   use Mix.Project
 
+  @description """
+  Elixir Internationalization library, extended to support translation files in the rails-i18n format.
+  """
+
+  @repo_url "https://github.com/change/linguist"
+
+  @version "0.2.1"
+
   def project do
     [
       app: :linguist,
-      version: "0.2.1",
+      version: @version,
       compilers: Mix.compilers() ++ [:cldr],
       elixir: "~> 1.6",
       deps: deps(),
-      package: [
-        contributors: ["Will Barrett, Chris McCord"],
-        licenses: ["MIT"],
-        links: %{github: "https://github.com/change/linguist"}
-      ],
-      description: """
-      Elixir Internationalization library, extended to support translation files in the rails-i18n format
-      """
+
+      # Hex
+      package: package(),
+      description: @description,
+
+      # Docs
+      name: "Linguist",
+      docs: [
+        extras: ["README.md"],
+        main: "readme",
+        source_ref: "v#{@version}",
+        source_url: @repo_url
+      ]
     ]
   end
 
@@ -25,11 +38,22 @@ defmodule Linguist.Mixfile do
     [applications: [:yaml_elixir]]
   end
 
+  def package do
+    [
+      contributors: ["Will Barrett, Chris McCord"],
+      licenses: ["MIT"],
+      links: %{github: @repo_url},
+      organization: "Change"
+    ]
+  end
+
   defp deps do
     [
       {:ex_cldr, "~> 2.0"},
       {:jason, "~> 1.0"},
       {:yaml_elixir, "~> 2.0"},
+
+      {:ex_doc, "~> 0.21.3", only: [:dev, :test], runtime: false},
       {:credo, "~> 0.9.0", only: [:dev, :test], runtime: false},
       {:sobelow, "~> 0.10", only: :dev, runtime: false}
     ]
